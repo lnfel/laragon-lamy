53fd7473217501d1d8c9bfd02d032b575d2c6522
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2021-08-30 16:40:02 +0800

    Test array loop

--- laragon.bat
+++ laragon.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enableDelayedExpansion
 
 :: https://stackoverflow.com/questions/2048509/how-to-echo-with-different-colors-in-the-windows-command-line
 :: https://stackoverflow.com/a/38617204
@@ -62,77 +63,93 @@ REM echo ^<ESC^>[7;31m                   [7;31minverse red foreground 
 REM echo ^<ESC^>[7m and nested ^<ESC^>[31m [7mbefore [31mnested [0m
 REM echo ^<ESC^>[31m and nested ^<ESC^>[7m [31mbefore [7mnested [0m
 
-setlocal
 :: Initialize local environment variables
 
 :: General
 Set downloads_path=C:\Users\Administrator\Downloads\
 
 :: Winrar
-Set winrar_link=https://www.rarlab.com/rar/winrar-x64-602.exe
-Set winrar_filename=winrar-x64-602.exe
-Set winrar_dir=%ProgramFiles%\WinRAR\
+Set item[0]=winrar
+Set link[0]=https://www.rarlab.com/rar/winrar-x64-602.exe
+Set filename[0]=winrar-x64-602.exe
+Set dir[0]=%ProgramFiles%\WinRAR\
+Set bin[0]=
+Set extract[0]=winrar-x64-602.exe /S
+Set app[0]=WinRAR.exe
+Set method[0]=Installing
 
 :: Laragon
-Set laragon_link=https://github.com/leokhoa/laragon/releases/download/5.0.0/laragon-portable.zip
-Set laragon_filename=laragon-portable.zip
-Set laragon_dir=C:\laragon\
+Set item[1]=laragon-portable
+Set link[1]=https://github.com/leokhoa/laragon/releases/download/5.0.0/laragon-portable.zip
+Set filename[1]=laragon-portable.zip
+Set dir[1]=C:\laragon\
+Set bin[1]=
+Set extract[1]="%dir[0]%app[0]" x -ibck %downloads_path%%filename[1]% *.* %dir[1]%
+Set app[1]=laragon.exe
+Set method[1]=Extracting
+
 :: Git
-Set git_link=https://github.com/git-for-windows/git/releases/download/v2.33.0.windows.2/PortableGit-2.33.0.2-64-bit.7z.exe
-Set git_filename=PortableGit-2.33.0.2-64-bit.7z.exe
-Set git_bin=C:\laragon\bin\git\bin
+Set item[2]=git-portable
+Set link[2]=https://github.com/git-for-windows/git/releases/download/v2.33.0.windows.2/PortableGit-2.33.0.2-64-bit.7z.exe
+Set filename[2]=PortableGit-2.33.0.2-64-bit.7z.exe
+Set dir[2]=C:\laragon\bin\git\
+Set bin[2]=bin\
+Set extract[2]=%downloads_path%%filename[2]% -y -o C:\laragon\bin\git
+Set app[2]=git.exe
+Set method[2]=Extracting
 
 :: Config files
 Set packages_conf=%laragon_dir%usr\packages.conf
 
 :: Begin Setup
-echo [33mDownloading[0m winrar
-::powershell -c "Invoke-WebRequest -Uri '%winrar_link%' -OutFile '%downloads_path%%winrar_filename%'"
-echo [32mDownloaded[0m winrar
-echo [33mInstalling[0m winrar
-::winrar-x64-602.exe /S
-echo [32mInstalled[0m winrar
+:: https://ss64.com/nt/for_l.html
+for %%i (0,1,2) do (
+	echo [36mChecking[0m !item[%%i]!
+	if exist !dir[%%i]!!bin[%%i]!!app[%%i]! (
+		echo [32mFound[0m !item[%%i]!
+	) else (
+		echo [33mDownloading[0m !item[%%i]!
+		powershell -c "Invoke-WebRequest -Uri '!link[%%i]!' -OutFile '%downloads_path%!filename[%%i]!'"
+		echo [32mDownloaded[0m !item[%%i]!
 
-echo [33mDownloading[0m laragon-portable
-::powershell -c "Invoke-WebRequest -Uri '%laragon_link%' -OutFile '%downloads_path%%laragon_filename%'"
-echo [32mDownloaded[0m laragon-portable
-echo [33mExtracting[0m laragon-portable
-::"%winrar_dir%WinRAR.exe" x -ibck %downloads_path%%laragon_filename% *.* %laragon_dir%
-echo [32mExtracted[0m laragon-portable
+		echo [33m!method[%%i]![0m !item[%%i]!
+		!extract[%%i]!
+		echo [32mInstalled[0m !item[%%i]!
+	)
+)
 
-echo [32mConfiguring[0m laragon packages
+:: Git config
+echo [33mConfiguring[0m git-portable
+%dir[2]%%bin[2]%%app[2]% config --global user.name "Dale Ryan Aldover"
+%dir[2]%%bin[2]%%app[2]% config --global user.email "bk2o1.syndicates@gmail.com"
+echo [32mGit user[0m & %dir[2]%%bin[2]%%app[2]% config --global user.name
+echo [32mGit email[0m & %dir[2]%%bin[2]%%app[2]% config --global user.email
+
+:: Laragon packages conf
+echo [33mConfiguring[0m laragon packages
 break>%packages_conf%
 (
-echo # PHP
-echo php-8.0.9=https://windows.php.net/downloads/releases/php-8.0.9-Win32-vs16-x64.zip
-echo # Apache
-echo apache-2.4.48=https://www.apachehaus.com/downloads/httpd-2.4.48-o111k-x64-vs16.zip
-echo # phpMyAdmin
-echo phpmyadmin-5.1.1=https://files.phpmyadmin.net/phpMyAdmin/5.1.1/phpMyAdmin-5.1.1-english.zip
-echo # MySQL
-echo mysql-8.0.26=https://cdn.mysql.com//Downloads/MySQL-8.0/mysql-8.0.26-winx64.zip
-echo # Node.js
-echo nodejs-14.17.5=https://nodejs.org/dist/v14.17.5/node-v14.17.5-win-x64.zip
-echo # Sublime
-echo sublime-4113=https://download.sublimetext.com/sublime_text_build_4113_x64.zip
-echo # Cmder
-echo cmder_mini-1.3.18=https://github.com/cmderdev/cmder/releases/download/v1.3.18/cmder_mini.zip
+	echo # PHP
+	echo php-8.0.9=https://windows.php.net/downloads/releases/archives/php-8.0.9-Win32-vs16-x64.zip
+	echo # Apache
+	echo apache-2.4.48=https://www.apachehaus.com/downloads/httpd-2.4.48-o111k-x64-vs16.zip
+	echo # phpMyAdmin
+	echo phpmyadmin-5.1.1=https://files.phpmyadmin.net/phpMyAdmin/5.1.1/phpMyAdmin-5.1.1-english.zip
+	echo # MySQL
+	echo mysql-8.0.26=https://cdn.mysql.com//Downloads/MySQL-8.0/mysql-8.0.26-winx64.zip
+	echo # Node.js
+	echo nodejs-14.17.5=https://nodejs.org/dist/v14.17.5/node-v14.17.5-win-x64.zip
+	echo # Sublime
+	echo sublime-4113=https://download.sublimetext.com/sublime_text_build_4113_x64.zip
+	echo # Cmder
+	echo cmder_mini-1.3.18=https://github.com/cmderdev/cmder/releases/download/v1.3.18/cmder_mini.zip
 ) > %packages_conf%
 echo [32mConfigured[0m laragon packages
-echo [33mDownloading[0m git-portable
-powershell -c "Invoke-WebRequest -Uri '%git_link%' -OutFile '%downloads_path%%git_filename%'"
-echo [32mDownloaded[0m git-portable
-echo [33mExtracting[0m git-portable
-%downloads_path%%git_filename% -y -o C:\laragon\bin\git
-echo [32mExtracted[0m git-portableecho [33mConfiguring[0m git-portable
-%git_bin%git.exe config --global user.name "Dale Ryan Aldover"
-%git_bin%git.exe config --global user.email "bk2o1.syndicates@gmail.com"
-echo [32mGit user[0m & %git_bin%git.exe config --global user.name
-echo [32mGit email[0m & %git_bin%git.exe config --global user.email
+
 :: https://stackoverflow.com/questions/46712814/get-current-users-path-variable-without-system-path-using-cmd::@For /F "Skip=2Tokens=1-2*" %%A In ('Reg Query HKCU\Environment /V PATH 2^>Nul') Do @Echo %%A=%%C
 @For /F "Skip=2Tokens=1-2*" %%A In ('Reg Query HKCU\Environment /V PATH 2^>Nul') Do Set user_path=%%C
 ::echo %user_path%
-SETX Path %user_path%C:\laragon\bin\sublime;
+::SETX Path %user_path%C:\laragon\bin\sublime;
 ::path C:\laragon\bin\sublime;%PATH%
 
 pause
