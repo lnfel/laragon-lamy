9abcbdb34f037a647dd256572fcc4277a38746f1
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2021-10-09 16:17:17 +0800

    Remove global user_path declaration

--- laragon.bat
+++ laragon.bat
@@ -132,8 +132,6 @@ Set directory[1]=C:\laragon\bin\php\php-8.0.9-Win32-vs16-x64
 Set directory[2]=C:\laragon\bin\composer
 Set directory[3]=C:\Users\Administrator\AppData\Roaming\Composer\vendor\bin
 
-Set user_path=
-
 for /L %%i in (0,1,3) do (
 	echo [36mChecking[0m !directory[%%i]!
 	for /F "Skip=2Tokens=1-2*" %%A In ('Reg Query HKCU\Environment /V PATH 2^>Nul') do (
