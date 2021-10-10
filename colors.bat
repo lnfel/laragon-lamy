a1bf1191f053a8404aa920bcd43f0ffa85d94cde
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2021-10-10 14:07:46 +0800

    Add description at the top

--- colors.bat
+++ colors.bat
@@ -1,5 +1,6 @@
 @echo off
 
+:: These are sample code for colored text output for cmd
 :: https://stackoverflow.com/questions/2048509/how-to-echo-with-different-colors-in-the-windows-command-line
 :: https://stackoverflow.com/a/38617204
 :: https://gist.githubusercontent.com/mlocati/fdabcaeb8071d5c75a2d51712db24011/raw/b710612d6320df7e146508094e84b92b34c77d48/win10colors.cmd
